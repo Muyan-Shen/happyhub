8dd9c9b612a768c9a2cdd3de2d4408ea107e2a05
ybq <1494873673@qq.com>
2023-12-04 19:06:56 +0800

    修改了userInsertVO，添加了username和password

--- src/main/java/cn/shenmuyan/vo/UserInsertVO.java
+++ src/main/java/cn/shenmuyan/vo/UserInsertVO.java
@@ -1,5 +1,11 @@
 package cn.shenmuyan.vo;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 /**
  * 用户注册
  * @Title: UserInsertVO
@@ -10,4 +16,10 @@ package cn.shenmuyan.vo;
  */
 
 public class UserInsertVO {
+    @NotBlank(message = "用户名不能为空")
+    private String username;
+    @NotBlank(message = "用户名不能为空")
+    @Length(min = 8,max = 16,message = "密码长度必须在6-16位之间")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$",message = "密码必须由6-16位数字字母下划线")
+    private String password;
 }
