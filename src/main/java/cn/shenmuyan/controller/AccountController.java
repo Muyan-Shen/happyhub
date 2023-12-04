be29f0a2a0705507df1f0ef7313e60f4be6851d0
Muyan-Shen <504142151@qq.com>
2023-12-04 15:20:45 +0800

    AccountController

---
+++ src/main/java/cn/shenmuyan/controller/AccountController.java
@@ -0,0 +1,17 @@
+package cn.shenmuyan.controller;
+
+/**
+ * 账号controller
+ * @Title: Account
+ * @Author ShenMuyan
+ * @Package cn.shenmuyan.controller
+ * @Date 2023/12/4 15:17
+ * @description:
+ */
+
+import org.springframework.stereotype.Controller;
+
+@Controller("user")
+public class AccountController {
+
+}
