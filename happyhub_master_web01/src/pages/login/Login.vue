af43cab1b019a60d0fa36cb1794573fb0b667697
ybq <1494873673@qq.com>
2023-12-09 22:05:17 +0800

    修改了登录界面的样式，上传了LOGO和PostMap图片。 打算把注册用tabs写到登录界面里，所以没上传注册的界面。

--- happyhub_master_web01/src/pages/login/Login.vue
+++ happyhub_master_web01/src/pages/login/Login.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="login">
-    <el-form :model="user"
-             :rules="rules"
-             ref="accountForm"
-             @submit="onLogin($event,$refs.accountForm)">
-      <el-form-item>
-        <h1>权限管理系统</h1>
-      </el-form-item>
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="user.username" clearable>请输入用户名</el-input>
-      </el-form-item>
-      <el-form-item label="密&emsp;码" prop="password">
-        <el-input v-model="user.passwordHash" show-password clearable>请输入密码</el-input>
-      </el-form-item>
-      <el-form-item>
-        <div class="btns">
-          <el-button type="primary" native-type="submit">Login</el-button>
-        </div>
-      </el-form-item>
-    </el-form>
+    <el-container>
+      <el-header>
+        <el-image src="LOGO.png"/>
+      </el-header>
+      <el-main>
+        <el-form :model="user"
+                 :rules="rules"
+                 ref="accountForm"
+                 @submit="onLogin($event,$refs.accountForm)">
+          <el-form-item>
+            <a>账号登录</a>
+          </el-form-item>
+          <el-form-item prop="username">
+            <el-input v-model="user.username"
+                      clearable>
+              <template #prepend>
+                <el-icon><User /></el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="user.passwordHash"
+                      show-password
+                      clearable>
+              <template #prepend>
+                <el-icon><Lock /></el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <div class="btns">
+              <el-button type="primary" native-type="submit">登录</el-button>
+            </div>
+          </el-form-item>
+        </el-form>
+      </el-main>
+      <el-footer>
+
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script setup>
+import {Lock, User} from "@element-plus/icons-vue";
 import {reactive,getCurrentInstance} from "vue";
 import {useRouter} from "vue-router";
 import {ElLoading} from "element-plus";
@@ -69,29 +91,61 @@ const onLogin = (e,form)=>{
 
 <style scoped lang="scss">
 .login{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
   height: 100%;
-  background: url("https://picsum.photos/1920/1080/?random") no-repeat center center;
-  .el-form{
-    min-width: 420px;
-    padding: 15px;
-    border-radius: 10px;
-    box-shadow: 0 0 10px #ccc;
-    background-color: rgba(255,255,255,0.4);
-    backdrop-filter: blur(2px);
-    .el-form-item{
-      h1{
-        text-align: center;
-        font-size: 36px;
-        font-weight: bold;
-        flex: 100%;
-      }
-      .btns{
-        flex: 1;
-        text-align: right;
+  display: flex;
+  .el-header{
+    height: 10%;
+    .el-image{
+      height: 75%;
+
+      margin-left: 8%;
+      margin-top: 1%;
+    }
+  }
+  .el-main{
+    background-color: #1a093d;
+    background-image: url("PostMap.jpg");
+    background-repeat: no-repeat;
+    background-position: 12% 60%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .el-form{
+      min-width: 280px;
+      padding: 20px;
+      margin-left: auto;
+      margin-right: 10%;
+      box-shadow: 0 0 10px #ccc;
+      background-color: rgba(255,255,255,1);
+      backdrop-filter: blur(2px);
+      .el-form-item{
+        margin-bottom: 8%;
+        a{
+          color: #7e51a6;
+          text-align: center;
+          font-size: 28px;
+          font-weight: bold;
+          flex: 100%;
+        }
+        .el-input{
+          height: 110%;
+          border-radius: 0px;
+        }
+        .btns{
+          flex: 1;
+          text-align: right;
+          margin-top: 3%;
+          .el-button{
+            width: 100%;
+            height: 36px;
+            background-color: #ff1c74;
+
+            font-size: 16px;
+            font-weight: 750;
+          }
+        }
       }
     }
   }
