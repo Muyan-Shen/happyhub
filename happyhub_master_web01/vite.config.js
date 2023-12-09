0e4d34bfd057ff65b470dc093aa8efb0259fe268
ybq <1494873673@qq.com>
2023-12-09 14:47:32 +0800

    在vite.config.js中添加了前台前端的运行端口。 npm install自动修改的package-lock.json中的插件版本。

--- happyhub_master_web01/vite.config.js
+++ happyhub_master_web01/vite.config.js
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    port:80,
+    host: '0.0.0.0',
+    open:true
+  },
+  resolve:{
+    alias:{
+      '@':'/src'
+    }
+  }
 })
