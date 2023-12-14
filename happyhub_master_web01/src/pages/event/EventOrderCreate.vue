d8fdf4a479f632a0513ee7298d4c189468eac9fa
Xunyi <1785367531@qq.com>
2023-12-14 16:26:00 +0800

    订单生成和更新时间的修改以及接收

--- happyhub_master_web01/src/pages/event/EventOrderCreate.vue
+++ happyhub_master_web01/src/pages/event/EventOrderCreate.vue
@@ -1,24 +1,43 @@
 <template>
-  显示订单  和确认订单按钮还有取消订单按钮
-  {{order}}
-  <el-button @click="orderConfirm1">确认</el-button>
-  <el-button @click="orderCancel1">取消</el-button>
+  <el-container>
+  <el-header>
+    <top-header/>
+  </el-header>
+  <el-main>
+  <div class="orderShow">
+    <el-card class="orderMessage" v-model="order">
+      <div class="orderTitle"><el-icon><Clock /></el-icon>发起订单</div>
+      <div>订单编号：{{order.id}}</div>
+      <div>用户号：{{order.userId}}</div>
+      <div>总价格：{{order.totalPrice}}元</div>
+      <div>活动编号：{{order.eventId}}</div>
+      <div>活动状态：{{order.status}}</div>
+      <div>订单创建时间：{{order.createAt}}</div>
+      <el-button @click="orderConfirm1">确认</el-button>
+      <el-button @click="orderCancel1">取消</el-button>
+    </el-card>
+
+  </div>
+  </el-main>
+    </el-container>
 </template>
 
 <script setup>
-import {ElMessage} from "element-plus";
+import {ElAlert, ElMessage} from "element-plus";
 import router from "../../config/router.config.js";
-import {getCurrentInstance, onMounted, ref} from "vue";
+import {getCurrentInstance, onMounted, reactive, ref} from "vue";
 import {useRoute} from "vue-router";
+import {Clock} from   "@element-plus/icons-vue";
+import topHeader from '../component/header.vue'
 
 const route = useRoute();
-const order=ref({
+const order= ref({
   "id":'',
   "userId":'',
   "eventId":'',
   "status":'',
   "totalPrice":'',
-  "createdAt":'',
+  "createAt":'',
   "updatedAt":'',
 })
 const $http = getCurrentInstance().appContext.config.globalProperties.$http;
@@ -30,10 +49,11 @@ const getOrder=(eventId,price)=> {
   $http.post('/pay/beforePaid/', {
     "eventId": eventId, "price": price
   }).then(res => {
+
     ElMessage(res.msg);
     orderId.value = res.data.id;
-    order.value=res.data;
-    console.log(order.value);
+    order.value = res.data;
+    console.log(order);
   })
 }
 
@@ -47,6 +67,17 @@ const orderConfirm1=()=>{
 }
 const orderCancel1=()=>{
   //异步请求
+  $http.get("/pay/orderCancel/"+orderId.value).then((res)=>{
+    ElMessage(res.msg);
+    router.push({
+      name:"home"
+    })
+  }).catch(err=>{
+    console.error('取消订单时出错：', err);
+    router.push({
+      name:"home"
+    })
+  })
 }
 
 
@@ -56,5 +87,21 @@ onMounted(()=>{
 </script>
 
 <style scoped lang="scss">
-
+.orderShow{
+  justify-content: center;
+  align-items: center;
+  margin-right: 200px;
+  margin-left: 200px;
+  padding: 0;
+  height: 100%;
+  .orderMessage {
+    .orderTitle {
+      background-color:hotpink ;
+      margin-bottom: 10px;
+      font-size: 25px;
+      font-weight: bold;
+      text-align: center;
+    }
+  }
+}
 </style>
